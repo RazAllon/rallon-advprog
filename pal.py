5644ce37138755355722498891a1aed820188029
Programming <programming@USProgramming2.local>
2017-09-20 14:10:38 -0400

    first commit

---
+++ pal.py
@@ -0,0 +1,26 @@
+def palindrome(n):
+str(n)
+string[::-1]
+
+
+
+
+print palindrome(8008)
+
+
+'''
+for  i in range (100,999):
+C=[]
+a=100
+if x==0:
+  a=a+1
+y=a*x
+y.string
+b=y.reverse
+if b==y:
+     C.append(y)
+if i=999 and a=999:
+    x==1
+C.reverse()
+    print C[0]
+    '''
---
+++ ra1000sum.py
@@ -0,0 +1,9 @@
+x=0
+for i in range(3,1001,3):
+    x=x+i
+for r in range(5,1001.5):
+    x=x+r
+print x
+for i in range(15,1000,15):
+    x=x-i
+print x
